9d5afdaacf367ab737709d5210ff0b80e2e431a6
rkddlsgur983 <rkddlsgur983@gmail.com>
2018-08-05 15:51:25 +0900

    페이지 추가
    sql테스트 페이지 추가

--- app/src/main/java/com/example/dobitnarae/MainActivity.java
+++ app/src/main/java/com/example/dobitnarae/MainActivity.java
@@ -1,37 +1,20 @@
 package com.example.dobitnarae;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button sqlBtn;
-    Button clothBtn;
-    Button storeBtn;
-    EditText storeEtxt;
-    public static final int RequestKey = 1001;
-    Intent intent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sqlBtn = (Button)findViewById(R.id.sqlBtn);
-        clothBtn = (Button)findViewById(R.id.clothBtn);
-        storeBtn = (Button)findViewById(R.id.storeBtn);
-        storeEtxt = (EditText)findViewById(R.id.storeEtxt);
-
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(myToolbar);
@@ -44,41 +27,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        sqlBtn.setOnClickListener(new View.OnClickListener() {
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(getApplicationContext(),SQLActivity.class);
-
-                startActivityForResult(intent,RequestKey);
-                //Toast.makeText(getApplicationContext(),"??",Toast.LENGTH_LONG).show();
-
+                Intent intent = new Intent(MainActivity.this, TestManagementActivity.class);
+                startActivity(intent);
             }
         });
-
-        clothBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {// 아이디에 해당하는 상점의 옷 가져오기
-                String str = storeEtxt.getText().toString();// 입력받은 아이디
-                intent = new Intent(getApplicationContext(),clothActivity.class);
-                intent.putExtra("ID",str);
-
-                startActivityForResult(intent,RequestKey);
-
-            }
-        });
-        storeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {// 아이디에 해당하는 상점의 옷 가져오기
-                String str = storeEtxt.getText().toString();// 입력받은 아이디
-                intent = new Intent(getApplicationContext(),storeActivity.class);
-                intent.putExtra("ID",str);
-
-                startActivityForResult(intent,RequestKey);
-
-            }
-        });
-
     }
-
-
 }

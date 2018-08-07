bf73e31d1f7ef668b49a6b165bfd3c270f666a0c
jong4876 <jong4876@users.noreply.github.com>
2018-08-08 02:38:55 +0900

    admin에 master복사

--- app/src/main/java/com/example/dobitnarae/StoreActivity.java
+++ app/src/main/java/com/example/dobitnarae/StoreActivity.java
@@ -1,33 +1,25 @@
 package com.example.dobitnarae;
 
-import android.content.res.Configuration;
-import android.support.design.widget.TabItem;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class ExampleSwipeMenu extends AppCompatActivity {
-    private Store store;
+public class StoreActivity extends AppCompatActivity {
+    Store store;
+    List<Clothes> items;
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -46,20 +38,29 @@ public class ExampleSwipeMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_example_swipe_menu);
+        setContentView(R.layout.activity_store);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+        ImageButton backButton = (ImageButton)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = (ViewPager) findViewById(R.id.container_clothes);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        // TODO 스크롤 하고 탭 클릭시 스크롤 처음으로 돌아가게하기
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.store_tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
@@ -73,14 +74,24 @@ public class ExampleSwipeMenu extends AppCompatActivity {
                         " 8개의 BK21사업팀으로 구성되어 있다. 학교법인 대양학원에 의해 운영된다. 현재 총장은 화학 박사 신구이다. ",
                 "24시간 영업","02-3408-3114", "신구",
                 37.550278,127.073114);
-    }
 
+        // 옷 정보들 가져와서 초기화
+        int ITEM_SIZE = 8;
+        items = new ArrayList<>();
+        Clothes[] item = new Clothes[ITEM_SIZE];
+        for(int i=0; i<ITEM_SIZE; i++){
+            item[i] = new Clothes(i, R.drawable.gobchang, "불곱창" + (i + 1),
+                    1000 * (i + 1), i % Constant.category_cnt + 1);
+            items.add(item[i]);
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_example_swipe_menu, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_store, menu);
+        return false;
     }
 
     @Override
@@ -115,36 +126,20 @@ public class ExampleSwipeMenu extends AppCompatActivity {
 
             // 액티비티 만들어서 케이스 문에다가 넣어주면 됩니다
             // 탭 이름은 values/string 에 들어있어요
-            // 아마 클래스 타입은 무조건 PlaceholderFragment 여야 할꺼같아요
 
             switch(position) {
                 case 0:
-                    return new StoreManagementFragment(store);
+                    return StoreInfoFragment.newInstance(0, store);
                 case 1:
-                    return new ItemManagementFragment();
-                case 2:
-                    return new OrderManagementFragment();
-                default:
-                    return null;
+                    return StoreClothesFragment.newInstance(1, items);
             }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // 탭 개수
-            return 3;
-        }
-    }
-
-    // 액티비티는 새로만들어지지 않고 유지
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            //Toast.makeText(this, "가로방향", Toast.LENGTH_LONG).show();
-        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            //Toast.makeText(this, "세로방향", Toast.LENGTH_LONG).show();
+            // Show 3 total pages.
+            return 2;
         }
     }
 }

16692a0b0aee856354d4340f5bb0983a8231c90d
rkddlsgur983 <rkddlsgur983@gmail.com>
2018-08-10 14:31:22 +0900

    클래스 재정의
    1. Store, Clothes 클래스 재정의 및 수정
    2. Order 프래그먼트 수정 시작

--- app/src/main/java/com/example/dobitnarae/Clothes.java
+++ app/src/main/java/com/example/dobitnarae/Clothes.java
@@ -1,27 +1,36 @@
 package com.example.dobitnarae;
 
-public class Clothes {
-    private int id;
-    private int image;
-    private String name;
-    private int price;
-    private int category;
+import java.io.Serializable;
 
-    int getImage() {
-        return this.image;
-    }
+public class Clothes implements Serializable{
+    private int cloth_id;
+    private int store_id;
+    private int type;
+    private String name;
+    private String intro;
+    private int price;
+    private int count;
+    private int sex;
+
+    int getCloth_id() { return this.cloth_id; }
+    int getStore_id() { return this.store_id; }
+    int getType(){ return this.type; }
     String getName() {
         return this.name;
     }
+    String getIntro(){return this.intro;}
     int getPrice() { return  this.price; }
-    int getId() { return this.id; }
-    int getCategory() { return this.category; }
+    int getCount() { return this.count; }
+    int getSex() { return this.sex; }
 
-    Clothes(int id, int image, String name, int price, int category) {
-        this.id = id;
-        this.image = image;
+    Clothes(int cloth_id,int store_id, int type, String name, String intro, int price, int count, int sex) {
+        this.cloth_id = cloth_id;
+        this.store_id = store_id;
+        this.type = type;
         this.name = name;
+        this.intro = intro;
         this.price = price;
-        this.category = category;
+        this.count = count;
+        this.sex = sex;
     }
 }
---
+++ app/src/main/java/com/example/dobitnarae/OrderManagementListViewAdapter.java
@@ -0,0 +1,7 @@
+package com.example.dobitnarae;
+
+import android.widget.BaseAdapter;
+
+public class OrderManagementListViewAdapter extends BaseAdapter {
+
+}
